fbcb7959dd9c3d87980d8d913b4cfae8805348f3
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-04-05 16:28:14 -0500

    Adds login mutation

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const { users, tasks } = require('../constants');
 const User = require('../database/models/user');
@@ -19,6 +20,24 @@ module.exports = {
         const newUser = new User({ ...input, password: hashedPassword });
         const result = await newUser.save();
         return result;
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
+    login: async (_, { input }) => {
+      try {
+        const user = await User.findOne({ email: input.email })
+        if(!user) {
+          throw new Error('User not found');
+        }
+        const isPasswordValid = await bcrypt.compare(input.password, user.password);
+        if(!isPasswordValid) {
+          throw new Error('Incorrect Password');
+        }
+        const secret = process.env.JWT_SECRET_KET || 'secretsnaughtforsharing'
+        const token = await jwt.sign({ email: user.email }, secret, { expiresIn: '1d' });
+        return { token };
       } catch (error) {
         console.log(error);
         throw error;
--- typeDefs/user.js
+++ typeDefs/user.js
@@ -8,6 +8,16 @@ module.exports = gql`
 
   extend type Mutation {
     signup(input: signupInput): User
+    login(input: loginInput): Token
+  }
+
+  input loginInput {
+    email: String!
+    password: String!
+  }
+
+  type Token {
+    token: String!
   }
 
   input signupInput {
